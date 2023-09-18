58123ca0ce90582299a4a3a401bcd18cd8d5b20d
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-18 21:04:08 +0100

    basic tailwind theme setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html","./src/**/*.{vue,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-one": '#FE34B9',
+        "primary-two": "#903AFF",
+        "secondary-major": "#150E28",
+        "secondary-minor": "#D434FE",
+      }
+    },
   },
   plugins: [],
 }
