56a448aecdc21026c044ccdd12610391d76bd3bf
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-18 23:10:43 +0100

    added custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ export default {
         "primary-two": "#903AFF",
         "secondary-major": "#150E28",
         "secondary-minor": "#D434FE",
-      }
+      },
+      fontFamily: {
+        "montserrat": ["Montserrat", "san-serif"]
+      },
+      fontSize: {
+        "desktop-heading-1": '80px',
+        "desktop-heading-2": '32px',
+        "desktop-heading-3": "16px",
+        "desktop-paragraph": "14px",
+        "desktop-subtext": "20px",
+        "mobile-paragraph": "13px"
+      },
     },
   },
   plugins: [],
