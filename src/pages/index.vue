4fc6b17d2cc74ccdef88339537b00da814c142a3
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-19 18:41:44 +0100

    added introdution section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,12 +2,14 @@
     <div class="w-full max-w-7xl mx-auto">
         <AppHeader />
         <AppHero />
+        <AppIntro />
     </div>
 </template>
 
 <script setup>
 import AppHeader from '../components/AppHeader/index.vue'
 import AppHero from '../components/AppHero/index.vue'
+import AppIntro from '../components/AppIntro/index.vue'
 </script>
 
 <style lang="scss" scoped></style>
