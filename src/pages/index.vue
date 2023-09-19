dd5a79bc995dc56113d22190a675f4a0a89411d5
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-19 19:16:20 +0100

    added rules and guidelines section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,7 @@
         <AppHeader />
         <AppHero />
         <AppIntro />
+        <AppRules />
     </div>
 </template>
 
@@ -10,6 +11,7 @@
 import AppHeader from '../components/AppHeader/index.vue'
 import AppHero from '../components/AppHero/index.vue'
 import AppIntro from '../components/AppIntro/index.vue'
+import AppRules from '@components/AppRules/index.vue'
 </script>
 
 <style lang="scss" scoped></style>
