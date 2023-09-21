22b5dd98f2eda237d71f5bba1f4d539d7c3c6c8f
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-21 07:23:14 +0100

    added axios

---
+++ src/utils/axios.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const Axios = axios.create({
+    baseURL: 'https://backend.getlinked.ai',
+    headers:{
+        'Content-Type':'application/json'
+    }
+})
+export default{
+    Axios
+}
