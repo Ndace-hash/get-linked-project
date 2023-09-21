f33fc3df0c1047761ca9674f13ea6c404029d4b4
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-21 17:47:05 +0100

    remove border from nav on contact and register pages

--- src/components/AppHeader/DesktopNavBar.vue
+++ src/components/AppHeader/DesktopNavBar.vue
@@ -1,31 +1,17 @@
 <template>
   <header
-    class="hidden lg:flex justify-between items-center border-b border-white border-opacity-20 px-16 h-[90px] pt-8 pb-2"
-  >
+    :class="['hidden lg:flex justify-between items-center border-b  border-opacity-20 px-16 h-[90px] pt-8 pb-2', $route.name != 'home' ? 'border-transparent' : 'border-white']">
     <div class="overflow-hidden w-[150px]">
       <img src="@assets/get_linked.svg" alt="Get linked logo" class="w-full" />
     </div>
     <nav class="flex gap-5 px-14 items-center">
-      <a href="#timeline" class="capitalize text-desktop-paragraph font-medium"
-        >Timeline</a
-      >
-      <a href="#intro" class="capitalize text-desktop-paragraph font-medium"
-        >Overview</a
-      >
-      <a href="#FAQ" class="capitalize text-desktop-paragraph font-medium"
-        >FAQs</a
-      >
-      <RouterLink
-        to="/contact"
-        class="capitalize text-desktop-paragraph font-medium"
-        >Contact</RouterLink
-      >
-      <RouterLink
-        to="/register"
-        class="capitalize text-desktop-paragraph bg-gradient-to-r from-primary-one to-primary-two font-medium text-center rounded-sm py-2 px-10 cursor-pointer"
-      >
-        Register</RouterLink
-      >
+      <a href="#timeline" class="capitalize text-desktop-paragraph font-medium">Timeline</a>
+      <a href="#intro" class="capitalize text-desktop-paragraph font-medium">Overview</a>
+      <a href="#FAQ" class="capitalize text-desktop-paragraph font-medium">FAQs</a>
+      <RouterLink to="/contact" class="capitalize text-desktop-paragraph font-medium">Contact</RouterLink>
+      <RouterLink to="/register"
+        class="capitalize text-desktop-paragraph bg-gradient-to-r from-primary-one to-primary-two font-medium text-center rounded-sm py-2 px-10 cursor-pointer">
+        Register</RouterLink>
     </nav>
   </header>
 </template>
--- src/components/AppHeader/MobileNavBar.vue
+++ src/components/AppHeader/MobileNavBar.vue
@@ -1,65 +1,34 @@
 <template>
   <header
-    class="flex items-center justify-between h-[100px] border-b border-white border-opacity-20 pt-4 px-[50px] lg:hidden w-full relative"
-  >
+    :class="[' flex items-center justify-between h-[100px] border-b border-opacity-20 pt-4 px-[50px] lg:hidden w-full relative', $route.name != 'home' ? 'border-transparent hidden' : 'border-white']">
     <div class="overflow-hidden w-[100px]">
       <img src="@assets/get_linked.svg" alt="Get linked logo" class="w-full" />
     </div>
-    <button
-      class="flex flex-col w-[30px] gap-1"
-      @click="() => (showMenu = true)"
-    >
-      <span
-        class="h-[4px] bg-white block w-1/2 shrink-0 self-start rounded-sm"
-      ></span>
+    <button class="flex flex-col w-[30px] gap-1" @click="() => (showMenu = true)">
+      <span class="h-[4px] bg-white block w-1/2 shrink-0 self-start rounded-sm"></span>
       <span class="h-[4px] bg-white block w-full shrink-0 rounded-sm"></span>
-      <span
-        class="h-[4px] bg-white block w-1/2 shrink-0 self-end rounded-sm"
-      ></span>
+      <span class="h-[4px] bg-white block w-1/2 shrink-0 self-end rounded-sm"></span>
     </button>
-    <div
-      :class="[
-        'absolute w-screen md:justify-center flex top-0 left-0 h-[600px] items-center bg-secondary-major z-50 ease-linear duration-300',
-        showMenu ? 'translate-x-0 opacity-100 ' : 'translate-x-full opacity-50',
-      ]"
-    >
+    <div :class="[
+      'absolute w-screen md:justify-center flex top-0 left-0 h-[600px] items-center bg-secondary-major z-50 ease-linear duration-300',
+      showMenu ? 'translate-x-0 opacity-100 ' : 'translate-x-full opacity-50',
+    ]">
       <nav class="flex flex-col gap-5 px-14 md:items-center">
-        <a
-          href="#timeline"
-          class="capitalize text-desktop-subtext font-medium"
-          @click="() => (showMenu = false)"
-          >Timeline</a
-        >
-        <a
-          href="#intro"
-          class="capitalize text-desktop-subtext font-medium"
-          @click="() => (showMenu = false)"
-          >Overview</a
-        >
-        <a
-          href="#FAQ"
-          class="capitalize text-desktop-subtext font-medium"
-          @click="() => (showMenu = false)"
-          >FAQs</a
-        >
-        <RouterLink
-          to="/contact"
-          class="capitalize text-desktop-subtext font-medium"
-          @click="() => (showMenu = false)"
-          >Contact</RouterLink
-        >
-        <RouterLink
-          to="/"
+        <a href="#timeline" class="capitalize text-desktop-subtext font-medium"
+          @click="() => (showMenu = false)">Timeline</a>
+        <a href="#intro" class="capitalize text-desktop-subtext font-medium"
+          @click="() => (showMenu = false)">Overview</a>
+        <a href="#FAQ" class="capitalize text-desktop-subtext font-medium" @click="() => (showMenu = false)">FAQs</a>
+        <RouterLink to="/contact" class="capitalize text-desktop-subtext font-medium" @click="() => (showMenu = false)">
+          Contact</RouterLink>
+        <RouterLink to="/"
           class="capitalize text-desktop-subtext bg-gradient-to-r from-primary-one to-primary-two font-medium text-center rounded-md py-3 px-10 mt-4"
-          @click="() => (showMenu = false)"
-        >
-          Register</RouterLink
-        >
+          @click="() => (showMenu = false)">
+          Register</RouterLink>
       </nav>
       <button
         class="absolute top-8 right-12 text-4xl text-white font-bold w-[40px] h-[40px] rounded-full flex items-center justify-center bg-gradient-to-t from-primary-one to-primary-two"
-        @click="() => (showMenu = false)"
-      >
+        @click="() => (showMenu = false)">
         <div class="bg-secondary-major rounded-full">
           <Close />
         </div>
