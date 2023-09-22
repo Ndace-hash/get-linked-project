c7b100bc2d9859bf17cb056c93d80de4799b9d55
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-22 15:11:38 +0100

    added radial gradient component

---
+++ src/components/AppRadialBackground/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+div {
+    background-image: radial-gradient(#44097D 0%,
+            #150E28 70%);
+    opacity: .9;
+    background-repeat: no-repeat;
+    width: 500px;
+    height: 500px;
+    z-index: -5;
+}
+</style>
