1b4ccfddef883e66d5a5860475426c41f15bdc92
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-22 09:56:19 +0100

    added stars to privacy section

--- src/components/AppPrivacy/index.vue
+++ src/components/AppPrivacy/index.vue
@@ -1,27 +1,27 @@
 <template>
   <section id="privacy" class="py-16 flex flex-col lg:flex-row lg:px-16">
-    <div class="flex flex-col items-center px-6 lg:w-4/5 lg:items-start">
+    <div class="flex flex-col items-center px-6 lg:w-4/5 lg:items-start relative">
+      <img src="@assets/images/gray_star.svg" alt="star"
+        class="absolute top-[-3%] right-[25%] w-[9px] lg:w-[20px] lg:top-[-1%] lg:right-[15%]">
+      <img src="@assets/images/purple_star.svg" alt="star"
+        class="absolute top-[20%] right-[15%] w-[20px] lg:w-[20px] lg:top-[10%] lg:right-[5%]">
+      <img src="@assets/images/purple_star.svg" alt="star"
+        class="absolute bottom-[20%] left-[0%] w-[20px] lg:w-[20px] lg:bottom-[22%] lg:left-[-7%]">
+
       <h2
-        class="font-clash-display text-desktop-subtext lg:text-desktop-heading-2 text-center lg:text-left leading-tight mb-4"
-      >
+        class="font-clash-display text-desktop-subtext lg:text-desktop-heading-2 text-center lg:text-left leading-tight mb-4">
         Privacy Policy and
         <br /><span class="text-secondary-minor">Terms</span>
       </h2>
-      <p class="text-center lg:text-left lg:text-desktop-paragraph">
+      <p class="text-center lg:text-left lg:text-desktop-paragraph opacity-75">
         Last updated on September 12, 2023
       </p>
-      <p
-        class="text-center lg:text-left lg:text-desktop-paragraph w-[80%] my-6"
-      >
+      <p class="text-center lg:text-left lg:text-desktop-paragraph w-[80%] my-6">
         Below are our privacy & policy, which outline a lot of goodies. it’s our
         aim to always take of our participant
       </p>
-      <article
-        class="border border-secondary-minor bg-[#D9D9D9] bg-opacity-5 py-12 rounded-md"
-      >
-        <p
-          class="text-center mt-6 mb-8 px-4 lg:text-left lg:text-desktop-paragraph lg:leading-loose"
-        >
+      <article class="border border-secondary-minor bg-[#D9D9D9] bg-opacity-5 py-12 rounded-md">
+        <p class="text-center mt-6 mb-8 px-4 lg:text-left lg:text-desktop-paragraph lg:leading-loose">
           At getlinked tech Hackathon 1.0, we value your privacy and are
           committed to protecting your personal information. This Privacy Policy
           outlines how we collect, use, disclose, and safeguard your data when
@@ -49,25 +49,26 @@
               </p>
             </div>
           </div>
-          <button
-            class="bg-gradient-to-r from-primary-one to-primary-two py-2 px-6 rounded-md self-center"
-          >
+          <button class="bg-gradient-to-r from-primary-one to-primary-two py-2 px-6 rounded-md self-center">
             Read More
           </button>
         </div>
       </article>
     </div>
     <div class="relative w-[80%] mx-auto pt-[200px] me-12">
-      <img
-        src="@assets/images/man_on_lock.png"
-        alt="an illustration of a man with a key standing on an open lock"
-        class="w-full"
-      />
-      <img
-        src="@assets/images/lock_and_shield.png"
-        alt="an illustration of a shield"
-        class="w-[90%] lg:w-full lg:top-[10%] absolute top-[15%] left-1/2 -translate-x-1/2 lg:-translate-x-1/3"
-      />
+      <img src="@assets/images/white_star.svg" alt="star"
+        class="absolute top-[30%] right-[20%] w-[9px] lg:w-[20px] lg:top-[25%] lg:right-[5%]">
+      <img src="@assets/images/purple_star.svg" alt="star"
+        class="absolute top-[28%] left-[-5%] w-[15px]  lg:w-[10px]  lg:top-[57%] lg:left-[30%]">
+      <img src="@assets/images/white_star.svg" alt="star"
+        class="absolute bottom-[12%] left-[20%] w-[9px] lg:w-[15px] lg:bottom-[30%] lg:left-[20%]">
+      <img src="@assets/images/gray_star.svg" alt="star"
+        class="absolute bottom-[11%] right-[0%] w-[9px] lg:w-[20px] lg:bottom-[28%] lg:right-[-10%]">
+
+      <img src="@assets/images/man_on_lock.png" alt="an illustration of a man with a key standing on an open lock"
+        class="w-full" />
+      <img src="@assets/images/lock_and_shield.png" alt="an illustration of a shield"
+        class="w-[90%] lg:w-full lg:top-[10%] absolute top-[15%] left-1/2 -translate-x-1/2 lg:-translate-x-1/3" />
     </div>
   </section>
 </template>
