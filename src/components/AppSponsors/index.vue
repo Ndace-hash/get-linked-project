9d2ce21c4a2268863b7de320f55a9928b2857bf2
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-22 09:13:15 +0100

    added stars to sponsors section

--- src/components/AppSponsors/index.vue
+++ src/components/AppSponsors/index.vue
@@ -3,11 +3,18 @@
     <h2 class="font-clash-display text-desktop-subtext lg:text-desktop-heading-2 text-center mb-3">
       Partneers and Sponsors
     </h2>
-    <p class="text-center w-[80%] lg:w-2/5">
+    <p class="text-center w-[80%] lg:w-2/5 mb-4">
       Getlinked Hackathon 1.0 is honored to have the following major companies
       as its partners and sponsors
     </p>
-    <div class="border border-secondary-minor lg:py-6 lg:px-9 mt-12 lg:scale-110">
+    <div class="border border-secondary-minor lg:py-6 lg:px-9 mt-12 lg:scale-110 relative">
+      <img src="@assets/images/purple_star.svg" alt="star"
+        class="absolute top-[0%] left-[5%] w-[12px] lg:w-[20px] lg:top-[-10%] lg:right-[10%]">
+      <img src="@assets/images/purple_star.svg" alt="star"
+        class="absolute top-[33%] right-[43%] w-[9px] lg:w-[15px] lg:top-[13%] lg:right-[43%]">
+      <img src="@assets/images/white_star.svg" alt="star"
+        class="absolute bottom-[2%] right-[15%] w-[12px] lg:w-[20px] lg:bottom-[10%] lg:right-[43%]">
+
       <div class="grid grid-cols-3 grid-rows-2 relative scale-75">
         <div
           class="w-[100px] md:w-[150px] lg:w-[200px] py-4 flex items-center justify-center border-r border-r-secondary-minor border-b border-b-secondary-minor">
