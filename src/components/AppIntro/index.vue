d9bef8b70e9373f109fa968e057773acff831727
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-22 03:45:20 +0100

    added stars for intro section

--- src/components/AppIntro/index.vue
+++ src/components/AppIntro/index.vue
@@ -1,10 +1,19 @@
 <template>
   <section id="intro"
-    class="flex flex-col py-8 items-center lg:flex-row lg:justify-center lg:px-20 lg:py-16 border-b border-b-white border-opacity-20">
-    <div class="flex items-center justify-center w-[80%] max-w-md lg:w-[40%]">
+    class="flex flex-col py-8 items-center gap-4 lg:flex-row lg:justify-center lg:px-20 lg:py-16 border-b border-b-white border-opacity-20 relative">
+
+    <div class="flex items-center justify-center w-[80%] max-w-md lg:w-[40%] relative">
+      <img src="@assets/images/purple_star.svg" alt="star"
+        class="absolute top-[35%] left-[5%] w-[10px] lg:w-[16px] lg:top-[40%] lg:left-[-10%]">
+      <img src="@assets/images/arrow.svg" alt="arrow"
+        class="absolute bottom-[0%] right-[50%] translate-x-[50%] translate-y-[50%] w-[30px] lg:w-[40px] lg:top-[90%] lg:right-[0]">
+
       <img src="@assets/images/The_big_idea.png" alt="Lightbulb with ideas" class="w-full" />
     </div>
-    <div class="mt-8 px-6 lg:w-[60%]">
+    <div class="mt-8 px-6 lg:w-[60%] relative">
+      <img src="@assets/images/purple_star.svg" alt="star"
+        class="absolute top-[10%] right-[13%] w-[9px] lg:w-[20px] lg:top-[15%] lg:right-[10%]">
+
       <h2 class="font-clash-display text-center text-desktop-subtext lg:text-left lg:text-desktop-heading-2 w-full">
         Introduction to getlinked
         <br /><span class="text-secondary-minor">tech hackathon 1.0</span>
