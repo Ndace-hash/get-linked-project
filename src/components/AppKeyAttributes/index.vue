a13d94c32960fcad8e345521b57cb103340ba6dd
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-22 03:44:13 +0100

    added stars for key attribute section

--- src/components/AppKeyAttributes/index.vue
+++ src/components/AppKeyAttributes/index.vue
@@ -1,15 +1,26 @@
 <template>
   <section id="intro"
-    class="flex flex-col py-8 items-center lg:flex-row lg:justify-center lg:px-20 lg:py-16 border-b border-b-white border-opacity-20">
-    <div class="flex items-center justify-center w-[80%] lg:w-[50%] h-full">
-      <img src="@assets/images/analysis.svg" alt="An illustration of a lady reading" class="w-full h-full" />
+    class="flex flex-col py-8 items-center lg:flex-row lg:justify-center lg:px-20 lg:py-16 border-b border-b-white border-opacity-20 gap-6">
+    <div class="flex items-center justify-center w-[80%] lg:w-[50%] h-full relative pt-10">
+      <img src="@assets/images/purple_star.svg" alt="star"
+        class="absolute -top-4 left-[45%] w-[10px] lg:w-[20px] lg:top-[-20%] lg:left-[15%]">
+      <img src="@assets/images/circle.svg" alt="star"
+        class="absolute lg:top-[-8%] lg:left-[5%]  lg:w-[100px] hidden lg:flex">
+      <img src="@assets/images/gray_star.svg" alt="star"
+        class="absolute bottom-[40%] right-[40%] w-[10px] lg:w-[20px] lg:bottom-[35%] lg:right-[40%]">
+
+
+      <img src="@assets/images/analysis.svg" alt="An illustration of a lady reading" class="w-full h-full lg:scale-125" />
     </div>
-    <div class="mt-2 px-6 lg:px-3 lg:w-[50%] flex flex-col items-center lg:items-start">
+    <div class="mt-2 px-6 lg:px-3 lg:w-[50%] flex flex-col items-center lg:items-start relative">
+      <img src="@assets/images/white_star.svg" alt="star"
+        class="absolute bottom-[4%] right-[13%] w-[10px] lg:w-[20px] lg:bottom-[1%] lg:left-[-13%]">
+
       <h2 class="font-clash-display text-center text-desktop-subtext lg:text-left lg:text-desktop-heading-2 w-full">
         Judging criteria
         <br /><span class="text-secondary-minor">Key attributes</span>
       </h2>
-      <p class="w-[90%] mx-auto text-center px-1 mt-3 lg:text-left lg:mx-0 text-desktop-paragraph mb-4 lg:w-full"
+      <p class="w-[90%] mx-auto text-center px-1 mt-3 lg:text-left lg:mx-0 text-[12px] leading-relaxed mb-4 lg:w-full"
         v-for="(a, index) in attributes" :key="index">
         <span class="text-primary-one font-bold text-[16px]">{{ a.attribute }}:</span>
         {{ a.text }}
