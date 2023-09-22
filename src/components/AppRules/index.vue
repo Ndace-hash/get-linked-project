3acda65ab90f072b5fb31236cfe55acb3db5183d
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-09-22 03:46:12 +0100

    added stars for rules section

--- src/components/AppRules/index.vue
+++ src/components/AppRules/index.vue
@@ -1,15 +1,23 @@
 <template>
   <section id="intro"
     class="flex flex-col py-8 items-center lg:flex-row-reverse lg:justify-center lg:px-20 lg:py-16 border-b border-b-white border-opacity-20">
-    <div class="flex items-center justify-center w-[80%] max-w-md lg:w-[40%]">
+    <div class="flex items-center justify-center w-[80%] max-w-md lg:w-[40%] relative">
+      <img src="@assets/images/white_star.svg" alt="star" class="absolute bottom-[50%] left-[5%] w-[10px] lg:hidden">
+
       <img src="@assets/images/lady_reading.svg" alt="An illustration of a lady reading" class="w-full" />
     </div>
-    <div class="mt-8 px-6 lg:w-[60%]">
+    <div class="mt-8 px-6 lg:w-[60%] relative">
+      <img src="@assets/images/gray_star.svg" alt="star"
+        class="absolute top-[25%] right-[10%] w-[10px] lg:w-[20px] lg:-top-4 lg:right-[45%]">
+      <img src="@assets/images/white_star.svg" alt="star"
+        class="absolute -bottom-4 left-[30%] w-[10px] lg:w-[20px] lg:-bottom-4 lg:left-[90%]">
+
+
       <h2 class="font-clash-display text-center text-desktop-subtext lg:text-left lg:text-desktop-heading-2 w-full">
         Rules and
         <br /><span class="text-secondary-minor">Guidelines</span>
       </h2>
-      <p class="w-[90%] mx-auto text-center px-1 mt-3 lg:text-left lg:mx-0 text-desktop-paragraph">
+      <p class="w-[90%] mx-auto text-center px-1 mt-3 lg:text-left lg:mx-0 text-desktop-paragraph leading-relaxed">
         Our tech hackathon is a melting pot of visionaries, and its purpose is
         as clear as day: to shape the future. Whether you're a coding genius, a
         design maverick, or a concept wizard, you'll have the chance to
